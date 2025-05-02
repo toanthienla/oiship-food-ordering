593f8de726c0907544c4524535ae492606bc22da
Han Nguyen <hancontam@gmail.com>
2025-05-02 15:41:32 +0700

    update db and db example
    advance database

--- OishipFoodOrdering_DBScript.sql
+++ OishipFoodOrdering_DBScript.sql
@@ -30,7 +30,7 @@ CREATE TABLE Customer (
     email NVARCHAR(100) NOT NULL UNIQUE,
     phone NVARCHAR(15) NOT NULL CHECK (phone LIKE '0%' AND LEN(phone) = 10 AND phone NOT LIKE '%[^0-9]%'),
     password NVARCHAR(60) NOT NULL,
-    address NVARCHAR(255) NOT NULL, --tao bang rieng
+    address NVARCHAR(255) NOT NULL, --Sau này sẽ tạo bảng riêng
     status_id INT NOT NULL FOREIGN KEY REFERENCES UserStatus(status_id),
     created_at DATETIME DEFAULT GETDATE()
 );
@@ -46,7 +46,7 @@ CREATE TABLE Shipper (
     driver_license NVARCHAR(12) NOT NULL CHECK (LEN(driver_license) BETWEEN 10 AND 12),
     driver_license_image VARBINARY(MAX),
     address NVARCHAR(255) NOT NULL,
-    vehicle_info NVARCHAR(100) NOT NULL, --bang so xe
+    vehicle_info NVARCHAR(100) NOT NULL, --Bảng số xe
     status_id INT NOT NULL FOREIGN KEY REFERENCES UserStatus(status_id),
     created_at DATETIME DEFAULT GETDATE()
 );
@@ -80,14 +80,14 @@ CREATE TABLE [Admin] (
 CREATE TABLE Category (
     category_id INT IDENTITY PRIMARY KEY,
     name NVARCHAR(100) NOT NULL,
-    restaurant_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     created_at DATETIME DEFAULT GETDATE()
 );
 
 --Bảng Menu (Chứa các MenuItem/Món ăn)
 CREATE TABLE Menu (
     menu_id INT IDENTITY PRIMARY KEY,
-    restaurant_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     available BIT DEFAULT 1,
     created_at DATETIME DEFAULT GETDATE()
 );
@@ -95,7 +95,7 @@ CREATE TABLE Menu (
 -- Bảng MenuItem (Món ăn)
 CREATE TABLE MenuItem (
     item_id INT IDENTITY PRIMARY KEY,
-    restaurant_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
 	menu_id INT NOT NULL FOREIGN KEY REFERENCES Menu(menu_id),
     name NVARCHAR(100) NOT NULL,
     description NVARCHAR(255),
@@ -127,7 +127,7 @@ CREATE TABLE CartItem (
 CREATE TABLE [Order] (
     order_id INT IDENTITY PRIMARY KEY,
     customer_id INT NOT NULL FOREIGN KEY REFERENCES Customer(customer_id),
-    restaurant_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NOT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     shipper_id INT NULL FOREIGN KEY REFERENCES Shipper(shipper_id),
     total_amount DECIMAL(10,2) NOT NULL,
     payment_method NVARCHAR(50) NOT NULL CHECK (payment_method IN ('online', 'offline')),
@@ -185,41 +185,41 @@ CREATE TABLE SupportTicket (
     ticket_id INT IDENTITY PRIMARY KEY,
     customer_id INT NULL FOREIGN KEY REFERENCES Customer(customer_id),
     shipper_id INT NULL FOREIGN KEY REFERENCES Shipper(shipper_id),
-    restaurant_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     subject NVARCHAR(100) NOT NULL,
     message NVARCHAR(MAX) NOT NULL,
     status NVARCHAR(50) NOT NULL CHECK (status IN ('open', 'pending', 'closed')),
     created_at DATETIME DEFAULT GETDATE(),
     updated_at DATETIME DEFAULT GETDATE(),
     CONSTRAINT chk_support_ticket CHECK (
-        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NULL AND restaurant_id IS NOT NULL)
+        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NULL AND restaurantmanager_id IS NOT NULL)
     )
 );
 
--- Bảng VerificationCode (OTP hệ thống người cho user xác nhận gmail khi tạo tài khoản)
+-- Bảng VerificationCode (OTP hệ thống gửi cho user xác nhận gmail khi tạo tài khoản)
 CREATE TABLE VerificationCode (
     verification_id INT IDENTITY PRIMARY KEY,
     customer_id INT NULL FOREIGN KEY REFERENCES Customer(customer_id),
     shipper_id INT NULL FOREIGN KEY REFERENCES Shipper(shipper_id),
-    restaurant_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+	restaurantmanager_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     code NVARCHAR(32) NOT NULL,
     plain_code NVARCHAR(6) NOT NULL,
     created_at DATETIME DEFAULT GETDATE(),
     expires_at DATETIME NOT NULL,
     is_used BIT NOT NULL DEFAULT 0,
     CONSTRAINT chk_verification_user CHECK (
-        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NULL AND restaurant_id IS NOT NULL)
+        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NULL AND restaurantmanager_id IS NOT NULL)
     )
 )
 
 -- Bảng phí nhà hàng (Dùng cho role Admin điều chỉnh phí)
 CREATE TABLE RestaurantFee (
     fee_id INT IDENTITY PRIMARY KEY,
-    restaurant_id INT FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     order_id INT FOREIGN KEY REFERENCES [Order](order_id),
     fee_amount DECIMAL(10,2),
     fee_type NVARCHAR(50), -- commission, service_fee
@@ -243,14 +243,14 @@ CREATE TABLE OrderNotification (
     order_id INT FOREIGN KEY REFERENCES [Order](order_id),
     customer_id INT NULL FOREIGN KEY REFERENCES Customer(customer_id),
     shipper_id INT NULL FOREIGN KEY REFERENCES Shipper(shipper_id),
-    restaurant_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
+    restaurantmanager_id INT NULL FOREIGN KEY REFERENCES RestaurantManager(restaurantmanager_id),
     message NVARCHAR(255),
     notification_type NVARCHAR(50), -- order_completed, order_cancelled
     is_read BIT DEFAULT 0,
     created_at DATETIME DEFAULT GETDATE(),
     CONSTRAINT chk_one_user_target CHECK (
-        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurant_id IS NULL) OR
-        (customer_id IS NULL AND shipper_id IS NULL AND restaurant_id IS NOT NULL)
+        (customer_id IS NOT NULL AND shipper_id IS NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NOT NULL AND restaurantmanager_id IS NULL) OR
+        (customer_id IS NULL AND shipper_id IS NULL AND restaurantmanager_id IS NOT NULL)
     )
 );
