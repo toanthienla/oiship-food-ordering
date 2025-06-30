283c0655c084f0022457e368fcb79cd0abf92351
Toan La <134460591+toanthienla@users.noreply.github.com>
2025-06-30 20:51:44 +0700

    Uncomment in Contact Admin Servlet

--- OishipFoodOrdering/src/main/java/controller/admin/ContactRequestsServlet.java
+++ OishipFoodOrdering/src/main/java/controller/admin/ContactRequestsServlet.java
@@ -1,23 +1,23 @@
-//package controller.admin;
-//
-//import dao.ContactDAO;
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.*;
-//import jakarta.servlet.annotation.*;
-//import model.Contact;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//@WebServlet("/admin/contact-requests")
-//public class ContactRequestsServlet extends HttpServlet {
-//
-//    private final ContactDAO contactDAO = new ContactDAO();
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        List<Contact> contacts = contactDAO.getAllContacts();
-//        request.setAttribute("contacts", contacts);
-//        request.getRequestDispatcher("/WEB-INF/views/admin/contact_requests.jsp").forward(request, response);
-//    }
-//}
+package controller.admin;
+
+import dao.ContactDAO;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.Contact;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/contact-requests")
+public class ContactRequestsServlet extends HttpServlet {
+
+    private final ContactDAO contactDAO = new ContactDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Contact> contacts = contactDAO.getAllContacts();
+        request.setAttribute("contacts", contacts);
+        request.getRequestDispatcher("/WEB-INF/views/admin/contact_requests.jsp").forward(request, response);
+    }
+}
